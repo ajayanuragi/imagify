2fb2e7532953008513b90568729a7f58b504b290
ajay anuragi <ajayanuragi44@gmail.com>
2025-08-02 19:16:40 +0530

    add login and signup

--- client/src/context/AppContextProvider.jsx
+++ client/src/context/AppContextProvider.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import AppContext from "./AppContext";
 
-const AppContextProvider = ({children}) => {
+const AppContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [showLogin, setShowLogin] = useState(false);
   const value = {
     user,
     setUser,
+    showLogin,
+    setShowLogin,
   };
-  return (
-    <AppContext.Provider value={value}>{children}</AppContext.Provider>
-  );
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
 export default AppContextProvider;
