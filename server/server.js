d387b72ae811b964d81f1a031ec40ccd20e08c77
ajay anuragi <ajayanuragi44@gmail.com>
2025-08-03 06:42:05 +0530

    add database connection

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+import connectDB from "./config/mongodb.js";
+const PORT = process.env.PORT || 4000;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API working...");
+});
+
+try {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log("Server running on PORT:" + PORT);
+  });
+} catch (error) {
+  console.error("Server startup aborted due to DB connection error", error);
+  process.exit(1);
+}
